a3dae139dba5bec3b665afffca5c462b2b567883
YoussefElharouch <elharouchyoussef1998@gmail.com>
2024-02-07 15:24:02 +0100

    add: (nav bar )

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -4,13 +4,13 @@ import { SidebarRoutes } from "./SidebarRoutes";
 export function Sidebar() {
   return (
     <div className="h-full border-r flex flex-col overflow-auto bg-white shadow-sm">
-      <div className="p-6">
+        <div className="p-6">
         <Logo />
+        </div>
         <div className="flex flex-col w-full">
           <SidebarRoutes/>
         </div>
       </div>
-    </div>
   )
 }
 
