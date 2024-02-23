3f859ab86520b705e8b48c69a19578f106774823
YoussefElharouch <elharouchyoussef1998@gmail.com>
2024-02-23 15:06:51 +0100

    add: (attachment form component)

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,7 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/prisma/db";
 import { auth } from "@clerk/nextjs";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 import { boolean } from "zod";
 import {TitleForm} from "./_components/title-form";
@@ -9,6 +9,7 @@ import DescriptionForm from "./_components/description-form";
 import ImageForm from "./_components/image-form";
 import CategoryForm from "./_components/category-form";
 import PriceForm from "./_components/price-form";
+import AttachmentForm from "./_components/attachment-form";
 
 
 interface CourseIdPageProps {
@@ -22,6 +23,13 @@ async function CourseIdPage({ params }: CourseIdPageProps) {
     const course = await db.course.findUnique({
         where: {
             id: params.courseId
+        },
+        include: {
+            attachments:{
+                orderBy:{
+                    createdAt: 'desc'
+                }
+            }
         }
     })
     
@@ -109,6 +117,17 @@ async function CourseIdPage({ params }: CourseIdPageProps) {
                         <PriceForm 
                         initialData={course}
                         courseId={course.id} />
+                    </div>
+
+                    <div>
+                    <div className="flex items-center gap-x-2">
+                            <IconBadge icon={File} />
+                            <h2 className="text-xl">Resources & Attachments </h2>
+                    </div>
+                    <AttachmentForm
+                    initialData = {course} 
+                    courseId = {course.id}
+                    />
                     </div>
                 </div>
             </div>
